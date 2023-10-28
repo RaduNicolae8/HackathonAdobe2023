bdc41c8891231aff6859454b20b85d59eb6a9699
Radu Petrachioiu <radunicolae14@yahoo.com>
2023-10-28 10:10:37 +0300

    FrontEnd init

---
+++ frontend/DonateCompass/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Donate Compass</h1>
+    </>
+  )
+}
+
+export default App
