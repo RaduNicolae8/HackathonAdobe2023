1579b90705ff8fbc3224bd08c9d649b544b05c2e
Vlad <oancea.vlad19@yahoo.com>
2023-10-28 16:25:26 +0300

    crud for normal posts and ong posts + filtering

---
+++ backend/src/main/java/com/TheDonateCompass/TheDonateCompass/Entity/PostONGEntity.java
@@ -0,0 +1,66 @@
+package com.TheDonateCompass.TheDonateCompass.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "post_ong")
+public class PostONGEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String productType;
+    private String title;
+    private String description;
+    private String severity;
+    private String location;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long ongId) {
+        this.userId = ongId;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ backend/src/main/java/com/TheDonateCompass/TheDonateCompass/Repository/PostONGRepository.java
@@ -0,0 +1,8 @@
+package com.TheDonateCompass.TheDonateCompass.Repository;
+
+import com.TheDonateCompass.TheDonateCompass.Entity.PostONGEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostONGRepository extends JpaRepository<PostONGEntity, Long> {
+
+}
